4d6f04c8bef911c32ef8bcfe869237b3e59dd57a
Giimyy <sergiulive2007@yahoo.com>
2023-10-20 00:53:40 +0300

    2nd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=59ccaaa6";
+const API_URL = "https://www.omdbapi.com?apikey=59ccaaa6";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
